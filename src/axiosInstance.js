9c27a88b0c80e22cd730491007ce8c54ba462a8a
yashdaga18 <email.yash.daga@gmail.com>
2023-09-30 14:01:15 +0530

    initial

--- src/axiosInstance.js
+++ src/axiosInstance.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'http://localhost:4000/', // Replace with your server's URL
+  baseURL: 'http://13.49.60.104:4000/', // Replace with your server's URL
 });
 
 export default instance;
--- src/pages/PostPage.js
+++ src/pages/PostPage.js
@@ -35,7 +35,7 @@ export default function PostPage() {
         </div>
       )}
       <div className="image">
-        <img src={`http://localhost:4000/${postInfo.cover}`} alt=""/>
+        <img src={`http://13.49.60.104:4000/${postInfo.cover}`} alt=""/>
       </div>
       <div className="content" dangerouslySetInnerHTML={{__html:postInfo.content}} />
     </div>
