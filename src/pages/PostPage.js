c90e74db4cc279cd1d25107492c202d034f22dde
yashdaga18 <email.yash.daga@gmail.com>
2023-09-30 14:09:33 +0530

    initial

--- src/pages/PostPage.js
+++ src/pages/PostPage.js
@@ -9,7 +9,7 @@ export default function PostPage() {
   const {userInfo} = useContext(UserContext);
   const {id} = useParams();
   useEffect(() => {
-    fetch(`http://localhost:4000/post/${id}`)
+    fetch(`http://13.49.60.104:4000/post/${id}`)
       .then(response => {
         response.json().then(postInfo => {
           setPostInfo(postInfo);
